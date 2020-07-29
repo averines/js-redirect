902423a007c6d4392b0a101d3fcf58d2947a91e6
Герман Аверин <averines@mail.ru>
2020-07-29 18:05:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let url = new URL(window.location.href) // получили текущий урл
-
 let paramsString = url.search.replace('?','') //получили строку со всеми параметрами
 paramsString = paramsString.split('&');
-
 var params = {}
-
-
 
 //превратили строку с параметрами в объект
 paramsString.map(param =>  {
@@ -13,14 +9,15 @@ paramsString.map(param =>  {
   params[paramsArr[0]] = paramsArr[1]
 })
 
-
 switch (params.ref) {
   case '111': 
     window.location.href = 'https://yandex.ru/search/?lr=213&text=первый'
     // window.open('https://ya.ru/', '_blank') // открыть в новой вкладке
+    break
 
-   case '222':
+  case '222':
     window.location.href = 'https://yandex.ru/search/?lr=213&text=второй'
+    break
 
   default:
     console.log( 'указанный параметр ref не известен' )
